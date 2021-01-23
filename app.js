b991301c9db671c45e0fe3e8087a72883e3fa6e2
romuald85 <papeboyedk@gmail.com>
2021-01-23 22:16:22 +0100

    Fix navbar after scroll header and work on my portfolio

---
+++ app.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('.navbar');
+const navOffsetTop = nav.offsetTop;
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= navOffsetTop){
+        nav.style.position = 'fixed';
+        nav.style.top = 0;
+        nav.style.width = '100%';
+    }else{
+        nav.style.position = 'static';
+    }
+})
